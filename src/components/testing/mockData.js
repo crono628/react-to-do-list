98ffb0493aa12b0a9146f0e828d4776ebf1a4825
crono628 <84817352+crono628@users.noreply.github.com>
2022-09-23 19:15:53 -0400

    refactor with vite

--- src/components/testing/mockData.js
+++
@@ -1,9 +0,0 @@
-let mockData = [
-  { task: 'vacuum', complete: false, list: 'cleaning' },
-  { task: 'windex', complete: false, list: 'cleaning' },
-  { task: 'dishes', complete: false, list: 'cleaning' },
-  { task: 'fart bill', complete: false, list: 'bills' },
-  { task: 'ralph bill', complete: false, list: 'bills' },
-];
-
-export default mockData;
